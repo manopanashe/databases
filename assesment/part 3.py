723863e4641d54c397484b3b61cd00557c0857b8
manopanashe <4manop68@solent.ac.uk>
2020-04-16 14:50:24 +0100

    commit

--- assesment/part 3.py
+++ assesment/part 3.py
@@ -1,5 +1,9 @@
 import sqlite3
-db = sqlite3.connect('C:\Users\manop\Documents\databases\assesment3.db')
+db = sqlite3.connect("C:/Users/manop/Documents/databases/assesment3")
 cursor = db.cursor()
 shopper_id = input("Enter shopper id:")
-sql_query = ("")
+sql_query = ("SELECT shopper_id \ FROM shoppers")
+cursor.execute(sql_query)
+shopper_id = cursor.fetchone()
+print(shopper_id)
+db.close()
